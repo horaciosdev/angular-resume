af716f68d9726b4ba67324b52a73c5a872c3c51a
Horacio <horacio.schumann@gmail.com>
2023-01-16 23:15:41 -0300

    SCSS improvements and theme-changer bug fix. Improved hover colors and fixed a bug in the theme-changer component that removed classes that shouldn't be removed from the body.

--- src/app/components/theme-changer/theme-changer.component.ts
+++ src/app/components/theme-changer/theme-changer.component.ts
@@ -6,6 +6,7 @@ import { Component, ElementRef } from '@angular/core';
   styleUrls: ['./theme-changer.component.scss'],
 })
 export class ThemeChangerComponent {
+  previousClass: string = '';
   themeClass: string = 'light-theme purple';
 
   constructor(private elementRef: ElementRef) {}
@@ -15,14 +16,21 @@ export class ThemeChangerComponent {
   }
 
   changeTheme(newThemeClass: string): void {
+    this.previousClass = this.themeClass;
     this.themeClass = newThemeClass;
     this.saveTheme(this.themeClass);
     this.setTheme();
   }
 
   setTheme() {
-    this.elementRef.nativeElement.ownerDocument.body.className =
-      this.themeClass;
+    let prevClasses = this.previousClass.split(' ');
+    prevClasses.forEach((c) => {
+      this.elementRef.nativeElement.ownerDocument.body.classList.remove(c);
+    });
+    let themeClass = this.themeClass.split(' ');
+    themeClass.forEach((c) => {
+      this.elementRef.nativeElement.ownerDocument.body.classList.add(c);
+    });
   }
 
   saveTheme(newClass: string): void {
@@ -32,6 +40,7 @@ export class ThemeChangerComponent {
   loadTheme(): void {
     let _themeClass = localStorage.getItem('themeClass');
     if (_themeClass !== null) {
+      this.previousClass = this.themeClass;
       this.themeClass = _themeClass;
       this.setTheme();
     }
